2fde7fa5eb0e53b24c45fd00da200d64d1bd57af
jcccn <jccuestc@gmail.com>
2015-05-19 12:22:51 +0800

    Update the ShareSDK V 2.11.0 2015-05-18

--- ShareSDK/Core/AGCommon.framework/Headers/CMJSONKit.h
+++ ShareSDK/Core/AGCommon.framework/Headers/CMJSONKit.h
@@ -5,54 +5,6 @@
 //  under the terms of the Apache License, Version 2.0, as specified below.
 //
 
-/*
- Copyright (c) 2011, John Engelhart
- 
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
- 
- * Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- 
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- 
- * Neither the name of the Zang Industries nor the names of its
- contributors may be used to endorse or promote products derived from
- this software without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/*
- Copyright 2011 John Engelhart
- 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- 
- http://www.apache.org/licenses/LICENSE-2.0
- 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
 #include <stddef.h>
 #include <stdint.h>
 #include <limits.h>
@@ -66,13 +18,12 @@
 #import <Foundation/NSError.h>
 #import <Foundation/NSObjCRuntime.h>
 #import <Foundation/NSString.h>
-#endif // __OBJC__
+#endif
  
 #ifdef __cplusplus
 extern "C" {
 #endif
   
-
 // For Mac OS X < 10.5.
 #ifndef   NSINTEGER_DEFINED
 #define   NSINTEGER_DEFINED
@@ -106,14 +57,6 @@ typedef unsigned int   NSUInteger;
 
 typedef NSUInteger AG_SKFlags;
 
-/*
-  JKParseOptionComments        : Allow C style // and /_* ... *_/ (without a _, obviously) comments in JSON.
-  JKParseOptionUnicodeNewlines : Allow Unicode recommended (?:\r\n|[\n\v\f\r\x85\p{Zl}\p{Zp}]) newlines.
-  JKParseOptionLooseUnicode    : Normally the decoder will stop with an error at any malformed Unicode.
-                                 This option allows JSON with malformed Unicode to be parsed without reporting an error.
-                                 Any malformed Unicode is replaced with \uFFFD, or "REPLACEMENT CHARACTER".
- */
-
 enum {
   AG_SKParseOptionNone                     = 0,
   AG_SKParseOptionStrict                   = 0,
@@ -137,8 +80,6 @@ typedef AG_SKFlags AG_SKSerializeOptionFlags;
 #ifdef    __OBJC__
 
 typedef struct AG_SKParseState AG_SKParseState; // Opaque internal, private type.
-
-// As a general rule of thumb, if you use a method that doesn't accept a JKParseOptionFlags argument, it defaults to JKParseOptionStrict
 
 @interface AG_SKJSONDecoder : NSObject {
   AG_SKParseState *parseState;
@@ -165,15 +106,12 @@ typedef struct AG_SKParseState AG_SKParseState; // Opaque internal, private type
 // Methods that return mutable collection objects.
 - (id)mutableObjectWithUTF8String:(const unsigned char *)string length:(NSUInteger)length;
 - (id)mutableObjectWithUTF8String:(const unsigned char *)string length:(NSUInteger)length error:(NSError **)error;
-// The NSData MUST be UTF8 encoded JSON.
 - (id)mutableObjectWithData:(NSData *)jsonData;
 - (id)mutableObjectWithData:(NSData *)jsonData error:(NSError **)error;
 
 @end
 
-////////////
 #pragma mark Deserializing methods
-////////////
 
 @interface NSString (AG_SKJSONDeserializing)
 - (id)objectFromJSONString;
@@ -185,7 +123,6 @@ typedef struct AG_SKParseState AG_SKParseState; // Opaque internal, private type
 @end
 
 @interface NSData (AG_SKJSONDeserializing)
-// The NSData MUST be UTF8 encoded JSON.
 - (id)objectFromJSONData;
 - (id)objectFromJSONDataWithParseOptions:(AG_SKParseOptionFlags)parseOptionFlags;
 - (id)objectFromJSONDataWithParseOptions:(AG_SKParseOptionFlags)parseOptionFlags error:(NSError **)error;
@@ -194,9 +131,7 @@ typedef struct AG_SKParseState AG_SKParseState; // Opaque internal, private type
 - (id)mutableObjectFromJSONDataWithParseOptions:(AG_SKParseOptionFlags)parseOptionFlags error:(NSError **)error;
 @end
 
-////////////
 #pragma mark Serializing methods
-////////////
   
 @interface NSString (AG_SKJSONSerializing)
 // Convenience methods for those that need to serialize the receiving NSString (i.e., instead of having to serialize a NSArray with a single NSString, you can "serialize to JSON" just the NSString).
